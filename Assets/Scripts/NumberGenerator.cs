5a3efd1bf44335a210e27ae8a40a88107b4232e2
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2025-05-04 14:12:41 -0700

    Chunks now generate random numbers
    from 1 - 10 random operation
    have to make surer they all fit onto the gate

---
+++ Assets/Scripts/NumberGenerator.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using Mono.Cecil;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NumberGenerator : MonoBehaviour
+{
+    [SerializeField]
+    public enum operations
+    {
+        multiply,
+        add,
+        divide,
+        subtract,
+    };
+    public int range = 10;
+    public const int NUMBEROFOPERATIONS = 4;
+    operations LeftGateOp;
+    operations RightGateOp;
+    int RightGateNum;
+    int LeftGateNum;
+    [SerializeField]
+    TextMeshProUGUI RightGateText;
+    [SerializeField]
+    TextMeshProUGUI LeftGateText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        LeftGateOp = (operations)Random.Range(0, NUMBEROFOPERATIONS+1);
+        RightGateOp = (operations)Random.Range(0, NUMBEROFOPERATIONS+1);
+        LeftGateNum = Random.Range(1, range+1);
+        RightGateNum = Random.Range(1, range+1);
+        LeftGateText.text = numberFormator(LeftGateOp, LeftGateNum);
+        RightGateText.text = numberFormator(RightGateOp, RightGateNum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    string numberFormator(operations operation, int num)
+    {
+        string formattedString = "";
+        if (operation == operations.add)
+            formattedString += "+";
+        else if (operation == operations.subtract)
+            formattedString += "-";
+        else if (operation == operations.multiply)
+            formattedString += "*";
+        else if (operation == operations.divide)
+            formattedString += "÷";
+        formattedString += num.ToString();
+        Debug.Log(formattedString);
+        return formattedString;
+    }
+}
